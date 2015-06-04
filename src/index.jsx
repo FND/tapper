491dcdbb0d84988aa624750886fe400e78addcf6
FND <fnd@localhost.localdomain>
2015-06-04 12:28:52 +0200

    switched to ES6 method shorthand
    cf. fb40079889b0942d54fd9d291eb3f2846044f03f

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ var React = require("react");
 
 module.exports = React.createClass({
 	displayName: "Tapper",
-	render: function() {
+	render() {
 		return <div className="tapper">
 			<h3>Tapper</h3>
 			<p>now playing: {this.props.src}</p>
